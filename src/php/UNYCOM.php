e34252a0ff3823ae29483231e4fa9799ef5b0d5c
Carsten Wallenhauer <cwallenhauer@la-isla.org>
2026-08-05 22:02:41 +0000

    Robustness improved

--- src/php/UNYCOM.php
+++ src/php/UNYCOM.php
@@ -291,7 +291,7 @@ final class UNYCOM{
         'Reference'=>'',
         'Full'=>''];
 
-    public const UNYCOM_REGEX='/(([A-Z]{3,5}[ ]{0,1}[0-9\-abPCFS]{0,3} [\-\–\—] ){0,1})([0-9]{4})([ ]{0,1}[A-Z]{1,2})([0-9]{5})([A-Z ]{0,5})([0-9]{0,2})([^0-9]{1}|$)/u';
+    public const UNYCOM_REGEX='/(([A-Z]{3,5}[  ]{0,1}[0-9\-abPCFS]{0,3}[  ][\-\–\—][  ]){0,1})([0-9]{4})([  ]{0,1}[A-Z]{1,2})([0-9]{5})([A-Z  ]{0,5})([0-9]{0,2})([^0-9]{1}|$)/u';
 
     private $unycom=self::UNYCOM_TEMPLATE;
 
